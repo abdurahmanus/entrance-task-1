6ba2cf1a2ec119cdf3cd9ded6247197e9eba4785
Dmitry Gladkikh <abdurahmanus@gmail.com>
2018-01-24 17:48:00 +0300

    edit mutations

--- graphql/resolvers/mutation.js
+++ graphql/resolvers/mutation.js
@@ -1,4 +1,4 @@
-const { models } = require('../../models');
+const { models, sequelize } = require('../../models');
 
 module.exports = {
   // User
@@ -7,15 +7,13 @@ module.exports = {
   },
 
   updateUser (root, { id, input }, context) {
-    return models.User.findById(id)
-            .then(user => {
-              return user.update(input);
-            });
+    return models.User.findById(id).then(user => {
+      return user.update(input);
+    });
   },
 
   removeUser (root, { id }, context) {
-    return models.User.findById(id)
-            .then(user => user.destroy());
+    return models.User.findById(id).then(user => user.destroy());
   },
 
   // Room
@@ -24,52 +22,55 @@ module.exports = {
   },
 
   updateRoom (root, { id, input }, context) {
-    return models.Room.findById(id)
-            .then(room => {
-              return room.update(input);
-            });
+    return models.Room.findById(id).then(room => {
+      return room.update(input);
+    });
   },
 
   removeRoom (root, { id }, context) {
-    return models.Room.findById(id)
-            .then(room => room.destroy());
+    return models.Room.findById(id).then(room => room.destroy());
   },
 
   // Event
   createEvent (root, { input, usersIds, roomId }, context) {
-    return models.Event.create(input)
-            .then(event => {
-              event.setRoom(roomId);
-
-              return event.setUsers(usersIds)
-                    .then(() => event);
-            });
+    return sequelize
+      .transaction(t =>
+        models.Event.create(input, { transaction: t })
+          .then(event => event.setRoom(roomId, { transaction: t }))
+          .then(event =>
+            event
+              .setUsers(usersIds, { transaction: t })
+              .then(eventsUsers => event)
+          )
+      )
+      .catch(_ => { throw Error('Something goes wrong'); });
   },
 
   updateEvent (root, { id, input }, context) {
-    return models.Event.findById(id)
-            .then(event => {
-              return event.update(input);
-            });
+    return models.Event.findById(id).then(event => {
+      return event.update(input);
+    });
   },
 
   removeUserFromEvent (root, { id, userId }, context) {
-    return models.Event.findById(id)
-            .then(event => {
-              event.removeUser(userId);
-              return event;
-            });
+    return models.Event.findById(id).then(event => {
+      return event.removeUser(userId).then(() => event);
+    });
+  },
+
+  addUserToEvent (root, { id, userId }, context) {
+    return models.Event.findById(id).then(event => {
+      return event.addUser(userId).then(() => event);
+    });
   },
 
   changeEventRoom (root, { id, roomId }, context) {
-    return models.Event.findById(id)
-            .then(event => {
-              event.setRoom(id);
-            });
+    return models.Event.findById(id).then(event => {
+      return event.setRoom(roomId).then(e => e);
+    });
   },
 
   removeEvent (root, { id }, context) {
-    return models.Event.findById(id)
-            .then(event => event.destroy());
+    return models.Event.findById(id).then(event => event.destroy());
   }
 };
