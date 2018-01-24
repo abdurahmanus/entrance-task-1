f051d8ac789c3f6a5c2280b426fce31290f14b6f
Dmitry Gladkikh <abdurahmanus@gmail.com>
2018-01-24 16:56:38 +0300

    fix missing users and room for event's queries

--- graphql/resolvers/index.js
+++ graphql/resolvers/index.js
@@ -1,20 +1,20 @@
-const GraphQLDate = require('graphql-date');
+const GraphQLDate = require("graphql-date");
 
-const query = require('./query');
-const mutation = require('./mutation');
+const query = require("./query");
+const mutation = require("./mutation");
 
-module.exports = function resolvers () {
+module.exports = function resolvers() {
   return {
     Query: query,
 
     Mutation: mutation,
 
     Event: {
-      users (event) {
-        event.getUsers();
+      users(event) {
+        return event.getUsers();
       },
-      room (event) {
-        event.getRoom();
+      room(event) {
+        return event.getRoom();
       }
     },
 
